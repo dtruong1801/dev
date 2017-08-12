e57a5eb3a2cbfa96a9498741e2ea6fd6bec2273d
Daniel Truong <dtruong@netapp.com>
2017-08-12 15:09:52 -0700

    Add i18N capability to web

--- src/test/java/com/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/DevopsApplicationTests.java
@@ -1,16 +1,28 @@
 package com.devops;
 
+import com.devops.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @SpringBootTest
 public class DevopsApplicationTests {
 
+	@Autowired I18NService i18NService;
+
 	@Test
 	public void contextLoads() {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected String don't match", expectedResult, actual);
 	}
 
 }
